0f0f1b0c7af6df0f2613a1eb46b4f189dffb54de
jasmeettt <jasmeetsalhotrawork@gmail.com>
2025-02-17 23:03:55 +0530

    Add initial implementation of Weather App with HTML, CSS, and JavaScript

--- Weather Api/script.js
+++ Weather Api/script.js
@@ -6,11 +6,48 @@ document.addEventListener("DOMContentLoaded", function () {
   const temperature = document.getElementById("temperature");
   const description = document.getElementById("description");
   const errorMessage = document.getElementById("error-message");
+  const API_KEY = "86dbb68250fde86dc66cb4ce2077e560";
 
-  getWeatherButton.addEventListener("click", function () {
+  getWeatherButton.addEventListener("click", async function () {
     const city = cityInput.value.trim();
-    if (city === "") return;
+    if (!city) return;
     console.log(city);
+
+    // it may throw an error
+    // sever/database is always in different states
+
+    try {
+      const weatherData = await getWeather(city);
+      displayWeather(weather);
+    } catch (error) {
+      // showError();
+    }
+
     cityInput.value = "";
   });
+
+  async function getWeather(city) {
+    // gets the data
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+    const response = await fetch(url);
+    console.log(response);
+    console.log(typeof response);
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+    const data = await response.json();
+    console.log(data);
+    return data;
+  }
+
+  function displayWeather(weather) {
+    // diplays the data
+    
+  }
+
+  function showError() {
+    // shows an error message
+    weatherInfo.classList.add("hidden");
+    errorMessage.classList.remove("hidden");
+  }
 });
