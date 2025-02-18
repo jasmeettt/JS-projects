5945bb1db77640785cd37811f60d4fa276c9cb0f
jasmeettt <jasmeetsalhotrawork@gmail.com>
2025-02-18 19:52:15 +0530

    Refactor weather fetching logic to use promise chaining and improve code readability

--- Weather Api/script.js
+++ Weather Api/script.js
@@ -8,44 +8,39 @@ document.addEventListener("DOMContentLoaded", function () {
   const errorMessage = document.getElementById("error-message");
   const API_KEY = "86dbb68250fde86dc66cb4ce2077e560";
 
-  getWeatherButton.addEventListener("click", async function () {
+  getWeatherButton.addEventListener("click", function () {
     const city = cityInput.value.trim();
     if (!city) return;
 
     // it may throw an error
     // sever/database is always in different states
-    try {
-      const weatherData = await getWeather(city);
-      displayWeather(weatherData);
-    } catch (error) {
-      showError();
-    }
+    getWeather(city).then(displayWeather).catch(showError);
     cityInput.value = "";
   });
 
-  async function getWeather(city) {
+  function getWeather(city) {
     // gets the data
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error("City not found");
+    return fetch(url).then((response) => {
+      if (!response.ok) {
+        throw new Error("City not found");
+      }
+      return response.json();
+    });
+
+    function displayWeather(weatherData) {
+      // diplays the data
+      cityName.textContent = `${weatherData.name}`;
+      temperature.textContent = `Temprature: ${weatherData.main.temp}°C`;
+      description.textContent = `Description: ${weatherData.weather[0].description}`;
+      weatherInfo.classList.remove("hidden");
+      errorMessage.classList.add("hidden");
     }
-    const data = await response.json();
-    return data;
-  }
 
-  function displayWeather(weatherData) {
-    // diplays the data
-    cityName.textContent = `${weatherData.name}`;
-    temperature.textContent = `Temprature: ${weatherData.main.temp}°C`;
-    description.textContent = `Description: ${weatherData.weather[0].description}`;
-    weatherInfo.classList.remove("hidden");
-    errorMessage.classList.add("hidden");
-  }
-
-  function showError() {
-    // shows an error message
-    weatherInfo.classList.add("hidden");
-    errorMessage.classList.remove("hidden");
+    function showError() {
+      // shows an error message
+      weatherInfo.classList.add("hidden");
+      errorMessage.classList.remove("hidden");
+    }
   }
 });
