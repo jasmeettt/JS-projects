2e7da6d53250483f533e30a3c272b6f215ecf7c5
jasmeettt <jasmeetsalhotrawork@gmail.com>
2025-02-20 22:31:40 +0530

    Enhance shopping cart functionality with item removal and total price display

--- Shopping Cart Project/script.js
+++ Shopping Cart Project/script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const cartItems = document.getElementById("cart-items");
   const emptyCartMessage = document.getElementById("empty-cart");
   const CartTotalMessage = document.getElementById("cart-total");
-  const totalPrice = document.getElementById("total-price");
+  const totalPriceDisplay = document.getElementById("total-price");
   const checkoutBtn = document.getElementById("checkout-btn");
 
   const product = [
@@ -26,11 +26,53 @@ document.addEventListener("DOMContentLoaded", function () {
 
   productList.addEventListener("click", (e) => {
     if (e.target.tagName === "BUTTON") {
-      emptyCartMessage.innerHTML = "";
       const productId = parseInt(e.target.dataset.id);
       const products = product.find((p) => p.id === productId);
-      cart.push(product);
-      console.log(cart);
+      cart.push(products);
+      renderCart();
     }
+  });
+  function renderCart() {
+    cartItems.innerHTML = "";
+    let totalPrice = 0;
+
+    if (cart.length > 0) {
+      emptyCartMessage.classList.add("hidden");
+      CartTotalMessage.classList.remove("hidden");
+
+      cart.forEach((item) => {
+        totalPrice += item.price;
+        const cartItem = document.createElement("div");
+        cartItem.innerHTML = `
+        <span>${item.name} - ${item.price.toFixed(2)}</span>
+        <button data-id="${item.id}">Remove</button>
+        `;
+        cartItems.appendChild(cartItem);
+        totalPriceDisplay.textContent = `${totalPrice.toFixed(2)}`;
+      });
+    } else {
+      emptyCartMessage.classList.remove("hidden");
+      totalPriceDisplay.textContent = `0.00`;
+    }
+  }
+
+  cartItems.addEventListener("click", function (e) {
+    if (e.target.tagName === "BUTTON") {
+      const productId = parseInt(e.target.dataset.id);
+      const index = cart.findIndex((item) => item.id === productId);
+      console.log(index);
+
+      if (index !== -1) {
+        cart.splice(index, 1);
+      }
+      renderCart();
+    }
+  });
+
+  checkoutBtn.addEventListener("click", () => {
+    cart.length = 0;
+    alert(`Checkout Successful, your total is: ${totalPriceDisplay.innerHTML}`);
+    renderCart();
+    CartTotalMessage.classList.add("hidden");
   });
 });
