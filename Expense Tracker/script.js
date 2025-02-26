496f85bd88d4ac48250a26aa16d1f2b06060c648
jasmeettt <jasmeetsalhotrawork@gmail.com>
2025-02-27 00:46:43 +0530

    add JavaScript file

---
+++ Expense Tracker/script.js
@@ -0,0 +1,9 @@
+const expenseForm = document.getElementById("expense-form");
+const expenseName = document.getElementById("expense-name");
+const expenseAmount = document.getElementById("expense-amount");
+const expenseList = document.getElementById("expense-list");
+const submit = document.getElementById("submit");
+const total = document.getElementById("total");
+const totalAmount = document.getElementById("total-amount");
+
+let expenses = [];
